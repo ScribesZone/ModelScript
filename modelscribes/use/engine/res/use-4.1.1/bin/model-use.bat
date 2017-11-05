a6c7dae42bcab2a8fa0a367600dba5265cfd1ac4
escribis <escribis@users.noreply.github.com>
2017-11-05 17:35:54 +0100

    rename local use engine to model-use

--- modelscribes/use/engine/res/use-4.1.1/bin/model-use.bat
+++ modelscribes/use/engine/res/use-4.1.1/bin/model-use.bat
@@ -4,4 +4,4 @@ rem This proxy batch file is needed for the BAD tests in /test/state
 rem %*    = all command line arguments
 rem %~dp0 = pathname of this batch file
 
-call "%~dp0\start_use.bat" %*
+call "%~dp0\model-start_use.bat" %*
