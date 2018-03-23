de0ddf6ed4cddabadaaf6d2e73d56fcde2d5e7a7
escribis <escribis@users.noreply.github.com>
2018-03-23 16:47:28 +0100

    Update plantuml to last version

--- modelscripts/diagrams/plantuml/engine/__init__.py
+++ modelscripts/diagrams/plantuml/engine/__init__.py
@@ -2,15 +2,19 @@
 
 import os
 
+PLANTUML_JAR_FILE='plantuml.1.2018.2.jar'
+
 class PlantUMLEngine(object):
 
     def __init__(self, checks=False, format='svg', outputDir='.'):
-        self.plantumlJar=os.path.join(os.path.dirname(__file__),'res','plantuml.jar')
+        self.plantumlJar=os.path.join(
+            os.path.dirname(__file__),'res',PLANTUML_JAR_FILE)
         if checks:
             if not os.path.isfile(self.plantumlJar):
                 raise EnvironmentError('cannot find %s'% self.plantumlJar)
             if os.system('java -version') != 0:
-                raise EnvironmentError('java is required but it seems that it is not installed')
+                raise EnvironmentError(
+                    'java is required but it seems that it is not installed')
         self.defaultFormat=format
         self.defaultOutputDir=outputDir
 
