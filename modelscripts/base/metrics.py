41078587543d6f33e83f07df8e8d9ddb85d337cf
escribis <escribis@users.noreply.github.com>
2018-05-31 00:02:52 +0200

    add a accumulate operation to metrics

--- modelscripts/base/metrics.py
+++ modelscripts/base/metrics.py
@@ -1,6 +1,7 @@
 # coding=utf-8
-
+from typing import List
 from collections import OrderedDict
+
 class Metric(object):
 
     def __init__(self, label, n, plural=None):
@@ -43,6 +44,21 @@ class Metrics(object):
         for metric in metrics.all:
             self.add(metric)
         return self
+
+    def accumulate(self, metricsList):
+        #type: (List[Metrics])->Metrics
+        """
+        Increment all metric with the values in the given
+        metrics. Add metric entry in case of new metric.
+        """
+        for metrics in metricsList:
+            for metric in metrics.all:
+                if metric.label in self.metricNamed:
+                    # the metric exists, add the new value
+                    self.metricNamed[metric.label]+=\
+                        metric.n
+                else:
+                    self.metricNamed[metric.label]=metric.n
 
     def __str__(self):
         return ''.join(
