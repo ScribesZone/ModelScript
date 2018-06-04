7507b5b181c7a10269b9b6a6c24a9c361c42835b
escribis <escribis@users.noreply.github.com>
2018-06-04 09:21:49 +0200

    comment scenarios and permissions

--- modelscripts/scripts/printers.py
+++ modelscripts/scripts/printers.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 # DO NOT REMOVE
 import modelscripts.scripts.usecases.printer
-import modelscripts.scripts.scenarios.printer
-import modelscripts.scripts.permissions.printer
+#import modelscripts.scripts.scenarios.printer
+#import modelscripts.scripts.permissions.printer
 import modelscripts.scripts.objects.printer
 import modelscripts.scripts.glossaries.printer
-import modelscripts.scripts.use_classes.printer
+import modelscripts.scripts.classes.printer
 # import modelscripts.scripts.accesses.printer
