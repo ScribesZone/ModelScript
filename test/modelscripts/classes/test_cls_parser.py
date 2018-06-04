16b51efe97460cd5d7705b5d99bb5fc812c7200a
escribis <escribis@users.noreply.github.com>
2018-06-04 09:23:45 +0200

    create test driver for classes

--- test/modelscripts/classes/test_cls_parser.py
+++ test/modelscripts/classes/test_cls_parser.py
@@ -5,7 +5,7 @@ from nose.plugins.attrib import attr
 import modelscripts
 from modelscripts.scripts.megamodels.printer.megamodels import MegamodelPrinter
 from modelscripts.metamodels import (
-    glossaries
+    classes
 )
 from test.modelscripts.assertions import (
     F, E, W, I, H,
@@ -14,34 +14,18 @@ from test.modelscripts.assertions import (
 )
 
 
-
-
-EXPECTED_ISSUES={
-    # 'gl-mega01.gls':    {
-    #     #'mgm.sem.Import.Allowed':1,
-    #     E:2,
-    #       level:'*',
-    #       'else':0
-    # },
-}
-EXPECTED_METRICS={
-    #     'myFile.gls': {
-    #         'myIssueLabel' : 3
-    #     }
-}
-
 def testGenerator_Assertions():
     res = checkAllAssertionsForDirectory(
-        relTestcaseDir='gls',
-        extension=['.gls'],
-        expectedIssuesFileMap=EXPECTED_ISSUES,
-        expectedMetricsFileMap=EXPECTED_METRICS)
+        relTestcaseDir='cls',
+        extension=['.cls'],
+        expectedIssuesFileMap={},
+        expectedMetricsFileMap={})
 
     for (file , expected_issue_map, expected_metrics_map) in res:
         yield (
             checkValidIssues,
             file,
-            glossaries.METAMODEL,
+            classes.METAMODEL,
             expected_issue_map,
             expected_metrics_map)
 
