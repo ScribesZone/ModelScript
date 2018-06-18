b47e28931c019d76539ca0512be9fbf6b185f366
escribis <escribis@users.noreply.github.com>
2018-06-18 17:44:07 +0200

    add MDigraph as a subclass of Digraph

--- modelscripts/tools/graphviz/__init__.py
+++ modelscripts/tools/graphviz/__init__.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 import os
 from typing import Dict, Text, Optional
 from graphviz import (
-    Digraph
+    Digraph,
+    Graph,
 )
 
 THIS_DIR=os.path.dirname(__file__)
@@ -11,26 +12,28 @@ IMAGE_DIR=os.path.join(RES_DIR,'images')
 
 class Mapping(object):
 
-    def __init__(self, prefix='id'):
-        self.prefix = prefix
+    def __init__(self, prefixId='id'):
+        self.prefixId = prefixId
         self.nextCounts=dict()
         self.objToId=dict()
         #type: Dict[Text, object]
         self.idToObj=dict()
         #type: Dict[object, Text]
 
-    def id(self, object, prefix=None):
+    def id(self, object, prefixId=None):
         #type: (object, Optional[Text]) -> Text
         if object in self.objToId:
             return self.objToId[object]
         else:
-            prefix = self.prefix if prefix is None else prefix
-            if prefix not in self.nextCounts:
+            prefixId = (
+                self.prefixId if prefixId is None
+                else prefixId)
+            if prefixId not in self.nextCounts:
                 count=1
             else:
-                count=self.nextCounts[prefix]+1
-            self.nextCounts[prefix] = count
-            id=prefix+str(count)
+                count=self.nextCounts[prefixId]+1
+            self.nextCounts[prefixId] = count
+            id=prefixId+str(count)
             self.objToId[object]=id
             self.idToObj[id]=object
             return id
@@ -38,6 +41,41 @@ class Mapping(object):
     def obj(self, id):
         #type: (Text) -> object
         return self.objToId[id]
+
+
+class MDigraph(Digraph, Mapping):
+
+    def __init__(self,
+                 name=None,
+                 comment=None,
+                 filename=None,
+                 directory=None,
+                 format=None,
+                 engine=None,
+                 encoding='utf-8',
+                 graph_attr=None,
+                 node_attr=None,
+                 edge_attr=None,
+                 body=None,
+                 strict=False,
+                 prefixId='id'):
+        Digraph.__init__(self,
+                 name=name,
+                 comment=comment,
+                 filename=filename,
+                 directory=directory,
+                 format=format,
+                 engine=engine,
+                 encoding=encoding,
+                 graph_attr=graph_attr,
+                 node_attr=node_attr,
+                 edge_attr=edge_attr,
+                 body=body,
+                 strict=strict)
+        Mapping.__init__(self,
+                prefixId=prefixId)
+
+
 
 
 def imagePath(relFilename):
