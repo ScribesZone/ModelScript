de56a4e997761551855d82c7ebed5c0f3e32eed5
escribis <escribis@users.noreply.github.com>
2018-06-18 12:13:32 +0200

    add graphviz printer

--- modelscripts/scripts/usecases/all.py
+++ modelscripts/scripts/usecases/all.py
@@ -2,5 +2,6 @@
 from modelscripts.scripts.usecases.parser import UsecaseModelSource
 import modelscripts.scripts.usecases.printer
 import modelscripts.scripts.usecases.plantuml
+import modelscripts.scripts.usecases.graphviz
 
 
