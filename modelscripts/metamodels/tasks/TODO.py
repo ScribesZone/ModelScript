cb19ad3302f0ce35478b3d0a18e1b5f23da83253
escribis <escribis@users.noreply.github.com>
2018-06-19 09:00:23 +0200

    add TODO concept graphviz

--- modelscripts/metamodels/tasks/TODO.py
+++ modelscripts/metamodels/tasks/TODO.py
@@ -1,3 +1,4 @@
 # TODO: Add concepts expression in metamodel
 # TODO: Add concepts expression in printer
+# TODO: Add concepts expression in graphviz
 # TODO: Add concepts expression in semantic parser
