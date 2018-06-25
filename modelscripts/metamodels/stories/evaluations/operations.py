9adbc91dc1d1071bf8005171eb85286158bfac1e
escribis <escribis@users.noreply.github.com>
2018-06-25 09:36:41 +0200

    add comment

--- modelscripts/metamodels/stories/evaluations/operations.py
+++ modelscripts/metamodels/stories/evaluations/operations.py
@@ -1,9 +1,21 @@
 # coding=utf-8
 """
-Code of the Scenario metamodel.
+Metamodel package representing the evaluation of operations.
+This goes with stories.evaluation.
+The evaluation of operation is not detailled, one operation
+after each other. This avoids repeating the hierarchy of
+Operation class, with only few additional information.
+For instance instead of keeping for the deletion of an
+object, the reference to this object (which depends on
+the evaluation), a generic OperationStepEvaluation is
+used instead. This is due to the fact that the details
+of the evaluation is not so important. What is important
+is the evaluation of "check" operations and the
+final state (stored in the StoryEvaluation).
 
-The global structure of this metamodel is as following::
-
+    StepEvaluation
+    <|-- OperationStepEvaluation
+         <|-- CheckStepEvaluation
 
 """
 
@@ -29,7 +41,9 @@ class OperationStepEvaluation(StepEvaluation):
     The evaluation of an operation. Note that this class
     is concrete while OperationStep is abstract. This is
     due to the fact that we make no difference between
-    many operations.
+    many operations. OperationStepEvaluation are
+    indeed instanciated for all operations apart
+    CheckStep which contains interesting information.
     """
 
     def __init__(self,
