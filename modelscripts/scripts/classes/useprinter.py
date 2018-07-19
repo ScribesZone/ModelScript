5913369bdfd78a5c709588e0fb8a68ef91ab9c1d
escribis <escribis@users.noreply.github.com>
2018-07-19 13:39:10 +0200

    implement printer for invariant and add traceability

--- modelscripts/scripts/classes/useprinter.py
+++ modelscripts/scripts/classes/useprinter.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals, print_function, absolute_import, \
     division
 
-from typing import Optional
-
+from typing import Optional, Dict
+from collections import OrderedDict
 from modelscripts.base.modelprinters import (
     ModelPrinterConfig,
     ModelSourcePrinter
@@ -11,6 +11,9 @@ from modelscripts.base.modelprinters import (
 from modelscripts.metamodels.classes import (
     METAMODEL,
     ClassModel
+)
+from modelscripts.base.grammars import (
+    AST
 )
 
 import logging
@@ -24,9 +27,7 @@ from modelscripts.base.printers import (
     indent
 )
 from modelscripts.metamodels.classes import (
-    ClassModel
-)
-
+    ClassModel)
 __all__ = [
     'UsePrinter',
 ]
@@ -38,18 +39,46 @@ class Config(AbstractPrinterConfig):
         )
 
 class UsePrinter(AbstractPrinter):
+
     def __init__(self,
-                 theModel,
-                 config=Config()):
-        #type: (ClassModel, Optional[ModelPrinterConfig]) -> None
+                 theModel):
+        #type: (ClassModel) -> None
         super(UsePrinter, self).__init__(config=Config())
+
+        self._oclLineToClassModelElement=OrderedDict()
+        #type: Dict[int, 'ClassModelElement']
+        # for a given line in the generated .use, the
+        # OCLLine object corresponding
+        # Only line that corresponds to the OCLLine are stored.
         assert theModel is not None
         self.theModel=theModel
         assert isinstance(theModel, ClassModel)
 
-    def save(self, outputFile):
-        with open(outputFile, "w") as f:
-            f.write(self.output)
+    def _putElement(self, element, useLine=None):
+        """
+        Associate the element to the current line number with
+        respect to the current output.
+        """
+        # If various elements are at the same line, the last wins
+        # This should not be a problem
+        assert element is not None
+        use_line=self.currentLineNo if useLine is None else useLine
+        self._oclLineToClassModelElement[use_line]=element
+        element.useOCLLineNo=use_line
+        return AST.nodeLine(element.astNode)
+
+    def getElement(self, useLineNo):
+        if useLineNo not in self._oclLineToClassModelElement:
+            return None
+        else:
+            return self._oclLineToClassModelElement[useLineNo]
+
+    def getCLSLine(self, useLineNo):
+        element=self.getElement(useLineNo)
+        if element is None:
+            return None
+        else:
+            return AST.nodeLine(element.astNode)
 
     def do(self):
         self.doUseModel(self.theModel)
@@ -58,6 +87,10 @@ class UsePrinter(AbstractPrinter):
     def doUseModel(self, model):
 
         self.outLine('model GenModel')
+
+        # Skip
+        #   - packages
+        #   - datatypes
 
         for e in model.enumerations:
             self.doEnumeration(e)
@@ -71,24 +104,34 @@ class UsePrinter(AbstractPrinter):
         for ac in model.associationClasses:
             self.doAssociationClass(ac)
 
-        # TODO: invariants, operationConditions, dataTypes
+        self.outLine('\nconstraints\n')
+        for i in model.invariants:
+            self.doInvariant(i)
+
         return self.output
 
     def doEnumeration(self, enumeration):
-        self.out('%s %s {' % (
+        enum_line=self._putElement(enumeration)
+        self.outLine('%s %s { --<<< %i' % (
             'enum',
-            enumeration.name))
+            enumeration.name,
+            enum_line))
         for (i,el) in enumerate(enumeration.literals):
-            self.doEnumerationLiteral(el)
-            if i+1< len(enumeration.literals):
-                self.outLine(',')
-        self.outLine('}')
+            sep=',' if i+1< len(enumeration.literals) else ''
+            self.doEnumerationLiteral(el, sep)
+        self.outLine('} --<<< %s (bottom)' % enum_line)
 
-    def doEnumerationLiteral(self, enumerationLiteral):
-        self.outLine(enumerationLiteral.name, indent=1)
+    def doEnumerationLiteral(self, enumerationLiteral, sep):
+        literal_line=self._putElement(enumerationLiteral)
+        self.outLine('%s%s --<<< %i' % (
+            enumerationLiteral.name,
+            sep,
+            literal_line),
+            indent=1)
         return self.output
 
     def doPlainClass(self, class_):
+        enum_line=self._putElement(class_)
         if class_.superclasses:
             sc = ('< '
                   +','.join(map(
@@ -99,46 +142,73 @@ class UsePrinter(AbstractPrinter):
             ('abstract' if class_.isAbstract else None),
             'class',
             class_.name,
-            sc])))
+            sc,
+            ' --<<< %i' % enum_line])))
 
-        if class_.attributes:
-            self.outLine('attributes', indent=1)
-            for attribute in class_.attributes:
+        if class_.ownedAttributes:
+            self.outLine(
+                'attributes --<<< %i (+1?)' % (enum_line)
+                , indent=1)
+            for attribute in class_.ownedAttributes:
                 self.doAttribute(attribute)
 
-        if class_.operations:
-            self.outLine('operations', indent=1)
-            for operation in class_.operations:
-                self.doOperation(operation)
+        self.outLine('end -- <<< %i (bottom)' % enum_line)
+        return self.output
 
-        # if class_.invariants:
-        #     for invariant in class_.invariants:
-        #         self.doInvariant(invariant)
-        self.outLine('end',
-                     linesAfter=1)
+    def doAttribute(self, attribute):
+        att_line=self._putElement(attribute)
+        self.outLine('%s %s %s --<<< %i' % (
+                attribute.name,
+                ':',
+                attribute.type.name,
+                att_line),
+            indent=2)
         return self.output
 
     def doPlainAssociation(self, association):
-        self.outLine('%s %s' % (
+        assoc_line=self._putElement(association)
+        self.outLine('%s %s --<<< %i' % (
             association.kind,
             association.name,
+            assoc_line
         ))
-        self.outLine('between', indent=1)
+        self.outLine('between --<<< %i (+1?)' % (
+            assoc_line+1),
+            indent=1)
         for role in association.roles:
             self.doRole(role)
-        self.outLine('end', linesAfter=1)
+        self.outLine('end --<<< %i (bottom)' % assoc_line)
+        return self.output
+
+    def doRole(self, role):
+        role_line=self._putElement(role)
+        cardinalities=''.join([
+            '[',
+            str(role.cardinalityMin),
+            '..',
+            '*' if role.cardinalityMax is None
+                else str(role.cardinalityMax),
+            ']'])
+        self.outLine('%s%s role %s --<<< %i' % (
+                     role.type.name,
+                     cardinalities,
+                     role.name,
+                     role_line)
+                     , indent=2)
         return self.output
 
     def doAssociationClass(self, associationClass):
+        assocl_line=self._putElement(associationClass)
         if associationClass.superclasses:
             superclass_names = [c.name for c in associationClass.superclasses]
             sc = ' < ' + ','.join(superclass_names)
         else:
             sc = ''
-        self.outLine('%s %s%s' % (
+        self.outLine('%s %s%s --<<< %i' % (
             'associationclass',
             associationClass.name,
-            sc))
+            sc,
+            assocl_line))
         self.outLine('between', indent=1)
         for role in associationClass.roles:
             self.doRole(role)
@@ -148,73 +218,35 @@ class UsePrinter(AbstractPrinter):
             for attribute in associationClass.attributes:
                 self.doAttribute(attribute)
 
-        if associationClass.operations:
-            self.outLine('operations', indent=1)
-            for operation in associationClass.operations:
-                self.doOperation(operation)
-
-        self.outLine('end', linesAfter=1)
+        self.outLine('end --<<< %i (bottom)' % assocl_line)
         return self.output
 
-    def doAttribute(self, attribute):
-        self.outLine('%s %s %s' % (
-                attribute.name,
-                ':',
-                attribute.type.name),
-            indent=2)
+    def doInvariant(self, invariant):
+        for ocl_inv in invariant.oclInvariants:
+            self.doOCLInvariant(ocl_inv)
         return self.output
 
-    def doOperation(self, operation):
-        self.doModelTextBlock(operation.description)
-        self.outLine('%s%s' % (
-                operation.signature,
-                ' =' if operation.hasImplementation
-                    else ''),
-            indent=1
-        )
-        if operation.hasImplementation:
-            self.outLine(indent('        ',operation.expression)+'\n')
-        self.doModelTextBlock(operation.description)
-
-        # for condition in operation.conditions:
-        #     self.doOperationCondition(condition)
+    def doOCLInvariant(self, oclInvariant):
+        # skip invariant with no body specified
+        if len(oclInvariant.oclLines)>=1:
+            context_line=self._putElement(oclInvariant.context)
+            self.outLine('context self:%s inv %s : --<<< %i' % (
+                oclInvariant.context.class_,
+                oclInvariant.name,
+                context_line))
+            for ocl_line in oclInvariant.oclLines:
+                self.doOCLLine(ocl_line)
         return self.output
 
-    # def doInvariant(self, invariant):
-    #     if invariant.class_ is None:
-    #         prefix_comment = ''
-    #         prefix_first = self.kwd('context ')
-    #         prefix_rest = '    '
-    #     else:
-    #         prefix_comment = '    '
-    #         prefix_first = '    '
-    #         prefix_rest  = '        '
-    #     self.doModelTextBlock(invariant.description)
-    #     self.outLine('%s%s%s %s:' % (
-    #         prefix_first,
-    #         self.kwd('existential ') if invariant.isExistential else '',
-    #         self.kwd('inv'),
-    #         invariant.name,
-    #     ))
-    #     self.doModelTextBlock(invariant.description)
-    #     self.out(indent(prefix_rest, invariant.expression)+'\n')
-    #     return self.output
-
-    def doRole(self, role):
-        cardinalities=''.join([
-            '[',
-            str(role.cardinalityMin),
-            '..',
-            '*' if role.cardinalityMax is None
-                else str(role.cardinalityMax),
-            ']'])
-        self.outLine('%s%s role %s' % (
-                     role.type.name,
-                     cardinalities,
-                     role.name )
-                     , indent=2)
-        return self.output
-
+    def doOCLLine(self, oclLine):
+        line_cls = AST.nodeLine(oclLine.astNode)
+        oclLine.useOCLLineNo=line_cls
+        # print('##'*10,str(oclLine),str())
+        self._putElement(oclLine)
+        self.outLine('%s -- <<< %i' % (
+            oclLine.textLine,
+            line_cls),
+            indent=1)
 
 
 # TODO:
