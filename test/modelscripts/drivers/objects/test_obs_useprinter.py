aab82416455ce7a885e3b3fd021a87fc228d964a
escribis <escribis@users.noreply.github.com>
2018-07-20 09:12:21 +0200

    update tests

--- test/modelscripts/drivers/objects/test_obs_useprinter.py
+++ test/modelscripts/drivers/objects/test_obs_useprinter.py
@@ -12,9 +12,9 @@ from modelscripts.tools.use.engine import (
     USEEngine)
 from modelscripts.base.printers import AbstractPrinterConfig
 import os
-import modelscripts.scripts.classes.parser
-from modelscripts.scripts.classes.useprinter import (
-    UsePrinter
+import modelscripts.scripts.objects.parser
+from modelscripts.scripts.stories.useprinter import (
+    UseStoryPrinter
 )
 from modelscripts.base.issues import (
     FatalError
@@ -25,46 +25,51 @@ from modelscripts.base.grammars import (
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('test.'+__name__)
 
-def testGenerator_cls_useprinter():
+def testGenerator_cls_obsprinter():
     test_dir=os.path.join(
-        TEST_CASES_DIRECTORY,'cls')
+        TEST_CASES_DIRECTORY,'obs')
 
     #--- test all files ----------------------
     files = [
         os.path.join(test_dir, f)
             for f in os.listdir(test_dir)
-            if f.endswith('.cls')
-            and f.endswith('buildings.cls')]
+            if f.endswith('.obs')
+            # and f.endswith('buildings.cls')
+    ]
 
     for filename in files:
         yield doPrintUse, filename
 
 
-
-
-
 def doPrintUse(filename):
 
     #--- parser: .obs -> system -------------------
-    source = modelscripts.scripts.classes.parser.ClassModelSource(
+    source = modelscripts.scripts.objects.parser.ObjectModelSource(
         fileName=filename,)
     if not source.isValid:
         print(('##'*40+'\n')*10)
         print('==> IGNORING INVALID MODEL')
         print(('##'*40+'\n')*10)
     else:
-        scm = source.classModel
+        obm = source.objectModel
 
-        use_file_path=Environment.getWorkerFileName(
+        soil_file_path=Environment.getWorkerFileName(
             filename,
-            extension='.use')
-
+            extension='.soil')
+        obm. XXX
         print('TST: '+'='*80)
-        print('TST: result in %s' % use_file_path)
+        print('TST: result in %s' % soil_file_path)
         print('TST: '+'='*80)
-        usePrinter = UsePrinter(scm)
+        story=obm.storyEvaluation.step
+        usePrinter = UseStoryPrinter(story)
         usePrinter.do()
-        usePrinter.save(use_file_path)
-        print('TST: .use generated')
-        # print('TST: '+'='*80)
-        # getUSEResults(usePrinter, use_file_path)
+        usePrinter.save(soil_file_path)
+        print('TST: .soil generated')
+
+
+        engine=USEEngine
+        engine.executeSoilFileAsTrace(
+            useFile=
+            soilFile=soil_file_path)
+
+        XXX
