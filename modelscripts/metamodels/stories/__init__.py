c41f5b7e0af3294226841298352b87f7f82385fa
escribis <escribis@users.noreply.github.com>
2018-08-03 17:04:34 +0200

    add IncludeStep + AbstractStoryCollection + AbstractStoryId + doc

--- modelscripts/metamodels/stories/__init__.py
+++ modelscripts/metamodels/stories/__init__.py
@@ -13,14 +13,22 @@ The global structure of this metamodel is as following::
         <|-- Story
         <|-- VerbStep
         <|-- TextStep
-    <|-- OperationStep  (defined in Operations)
+    <|-- IncludeStep
+    <|-- OperationStep ... (defined in Operations)
+        <|-- ...
+
+    IncludeStep
+        ---> 1 AbstractStoryId
+
+    AbstractStoryCollection
+
 
 The hierarchy of step is recursive but Story is the only root.
 """
 
 
-from typing import Optional, List
-from abc import ABCMeta
+from typing import Optional, List, Text, Union
+from abc import ABCMeta, abstractproperty, abstractmethod
 from modelscripts.megamodels.elements import SourceModelElement
 from modelscripts.metamodels.permissions.sar import Subject
 
@@ -31,6 +39,10 @@ META_CLASSES=(
     'CompositeStep'
     'TextStep',
     'VerbStep',
+    'IncludeStep',
+    'AbstractStoryId',
+    'AbstractStoryCollection',
+    'EmptyStoryCollection'
 )
 
 __all__=META_CLASSES
@@ -39,13 +51,16 @@ DEBUG=3
 
 
 class Step(SourceModelElement, Subject):
-    __metaclass__ = ABCMeta
     """
     Abstract class for all steps.
-    Deal with 
-    (1) hierachy through "parent" management. 
-    (2) accesses with "superSubjects" / "subjecLabel"
+    This class deal with
+    (1) the hierarchy through "parent" management.
+    (2) accesses with "superSubjects" / "subjectLabel"
+    (3) hasOperations to indicates if (a) the step is an
+        operation or if (b) there is an operation in its
+        substeps (if any).
     """
+    __metaclass__ = ABCMeta
 
     def __init__(self,
                  model,
@@ -66,6 +81,10 @@ class Step(SourceModelElement, Subject):
         if parent is not None:
             self.parent.steps.append(self)
 
+    # @abstractproperty
+    # def hasOperations(self):
+    #     raise NotImplementedError('hasOperations')
+
     @property
     def superSubjects(self):
         """ Direct parents """
@@ -82,9 +101,11 @@ class Step(SourceModelElement, Subject):
 
 class CompositeStep(Step):
     """
-    Composite steps have substeps and therefore a steps attribute.
-    Only composites have steps but all steps have a parent (see Steps).
-    (the parent of step is None for Root)
+    Composite steps have substeps and therefore a "steps"
+    attribute.
+    Only composites have (nested) steps but all steps have
+    a parent (see Steps).
+    The parent of step is None for Stories.
     """
     __metaclass__ = ABCMeta
 
@@ -103,10 +124,28 @@ class CompositeStep(Step):
 
         self.steps=[]
         #type: List[Step]
+    #
+    # @property
+    # def hasOperations(self):
+    #     for substep in self.steps:
+    #         if substep.hasOperations:
+    #             return True
+    #     else:
+    #         return False
 
 
 class Story(CompositeStep):
-    """ The Story, that is the root of the Step hierarchy """
+    """
+    A story, that is, a root of a Step hierarchy.
+    Note that multiple stories can exists in the
+    context of AbstractStoryCollection.
+    Since they are rool, all stories have parent
+    is None.
+    NOTE: while a Story is a root, this is not
+    the case for StoryEvaluation since these can
+    be included. See StoryEvaluation for more
+    details.
+    """
 
     def __init__(self,
                  model,
@@ -114,11 +153,12 @@ class Story(CompositeStep):
                  lineNo=None,
                  description=None):
         # """
-        # Create the story. The model might be unknown at this
-        # time, but it can be set later. Subclasses of step
-        # will compute model dynamically (at initialization time
-        # of each step) so any update is ok (as long as steps are
-        # created after the definition of model, which is ok).
+        # Create the story.
+        # The "model" might be unknown at this time, but it can
+        # be set later. Subclasses of Step will compute "model"
+        # at initialization time (e.g. "self.model = parent.model").
+        # As long as the attribute "model" is defined for the root
+        # (the Story), this is ok.
         # """
         super(Story, self).__init__(
             model=model,
@@ -158,8 +198,9 @@ class TextStep(CompositeStep):
 
 
 class VerbStep(CompositeStep):
-
-    """ A step with a subject and verb, plus steps in this block"""
+    """
+    A step with a subject and verb, plus steps in the block.
+    """
     def __init__(self,
                  parent,
                  subjectName,
@@ -178,3 +219,76 @@ class VerbStep(CompositeStep):
         self.verbName=verbName
 
 
+class AbstractStoryId(object):
+    """
+    Identifier of a story. Concrete classes must
+    be defined in metamodel reusing "stories".
+    (Abstract)StoryIds are used by IncludeStep
+    to identify which story should be included.
+    This class is also used by AbstractStoryCollection.
+    """
+    __metaclass__ = ABCMeta
+
+    pass
+
+
+class IncludeStep(Step):
+    """
+    A step corresponding to the inclusion of a story.
+    Only the kind/name of the story is stored. That story
+    associated with this name depends on the environment
+    given to the evaluator.
+    """
+    def __init__(self,
+                 parent,
+                 storyId,
+                 words=(),
+                 astNode=None,
+                 lineNo=None,
+                 description=None):
+        #type: (Step, AbstractStoryId, Optional['ASTIncludeStep'], Optional[int], Optional['TextBlock']) -> None
+        super(IncludeStep, self).__init__(
+            model=parent.model,
+            parent=parent,
+            astNode=astNode,
+            lineNo=lineNo,
+            description=description)
+
+        self.storyId=storyId
+        #type: AbstractStoryId
+        # The story id. This corresponds to the logical/internal
+        # representation.
+
+        self.words=words
+        #type: List[Text]
+        # The external, concrete syntax.
+        # This could be used for printing or error message.
+
+
+class AbstractStoryCollection(object):
+    """
+    Abstract class describing collections of stories indexed
+    by AbstractStoryId.
+    In practice and currently there is only one AbstractStoryCollection.
+    The "stories" package does not provided any concrete implementation.
+    There is no syntactic elements in the "stories" package to
+    represent this collection. So this is in the metamodel but not in
+    the syntax.
+    The "scenarios" package fill this gap, providing
+    a concrete implementation for AbstractStoryCollection.
+    """
+    @abstractmethod
+    def story(self, storyId):
+        #type: (AbstractStoryId) -> Optional[Story]
+        """
+        Return for a given story id, the corresponding story.
+        If there is no such story available for this story then
+        return None.
+        """
+        raise NotImplementedError('story() not implemented')
+
+
+class EmptyStoryCollection(AbstractStoryCollection):
+
+    def story(self, storyId):
+        return None
