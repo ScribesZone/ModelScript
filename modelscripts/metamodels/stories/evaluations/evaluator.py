8c1096efe60f2239b412cefb7b520128752d1a0f
escribis <escribis@users.noreply.github.com>
2018-08-03 17:11:08 +0200

    add eval_include()

--- modelscripts/metamodels/stories/evaluations/evaluator.py
+++ modelscripts/metamodels/stories/evaluations/evaluator.py
@@ -1,11 +1,13 @@
 # coding=utf-8
 """
-Engine performing the evaluation of stories. It basically creates
-a StoryE evaluation from
+Engine performing the evaluation of stories.
+The engine basically creates a StoryEvaluation from
+an initial state (an ObjectModel). This state is updated
+inplace.
 """
 
 from __future__  import print_function
-from typing import Union, Optional
+from typing import Union, Optional, Dict, Text
 from modelscripts.base.grammars import (
     ASTNodeSourceIssue
 )
@@ -16,10 +18,14 @@ from modelscripts.scripts.textblocks.parser import (
     astTextBlockToTextBlock
 )
 from modelscripts.metamodels.stories import (
+    # CompositeStory,
     Story,
+    IncludeStep,
     Step,
     TextStep,
     VerbStep,
+    AbstractStoryId,
+    AbstractStoryCollection
 )
 from modelscripts.metamodels.stories.operations import (
     ObjectCreationStep,
@@ -54,7 +60,9 @@ from modelscripts.metamodels.permissions import (
 from modelscripts.metamodels.stories.evaluations import (
     StepEvaluation,
     CompositeStepEvaluation,
-    StoryEvaluation
+    StoryEvaluation,
+    StoryIncludeEvaluation
+    # CompositeStoryEvaluation
 )
 from modelscripts.metamodels.stories.evaluations.operations import (
     OperationStepEvaluation,
@@ -74,30 +82,127 @@ ISSUES={
     'LINK_NO_SOURCE':'st.eval.Link.NoSource',
     'LINK_NO_TARGET':'st.eval.Link.NoTarget',
     'LINKDEL_NOT_IMPL':'st.eval.LinkDeletion.NoImpl',
+    'NO_STORY':'st.eval.Include.NoStory',
 }
 def icode(ilabel):
     return ISSUES[ilabel]
 
+# class StoryCollection(object):
+#
+#     def __init__(self,
+#                  objectsStory=None,
+#                  contextMap={},
+#                  storyMap={}):
+#
+#         self.objectStory=objectsStory
+#         #type: Optional[Story]
+#
+#         self.contextMap=contextMap
+#         #type: Dict[Text, Story]
+#
+#         self.storyMap=storyMap
+#         #type: Dict[Text, Story]
+#
+#     def setStory(self, kind, name, story):
+#         if kind=='object model':
+#             self.objectStory=story
+#         elif kind in ['context']:
+#             self.contextMap[name]=story
+#         elif kind=='story':
+#             self.storyMap[name]=story
+#         else:
+#             raise NotImplementedError('unknown story kind: "%s".' % kind)
+#
+#     def getStory(self, kind, name):
+#         #type: (Text, Optional[Text]) -> Optional[Story]
+#         """
+#         Return the story associated with the kind and name.
+#         Return None if the story does not exists.
+#         In that case use getGetIssueMessage.
+#         Name is not taken into account for "object model" kind.
+#         Providing None is OK.
+#         """
+#         if kind=='object model':
+#             return self.objectStory
+#         elif kind=='context':
+#             if name in self.contextMap:
+#                 return self.contextMap[name]
+#             else:
+#                 return None
+#         elif kind=='story':
+#             if name in self.storyMap:
+#                 return self.storyMap[name]
+#             else:
+#                 return None
+#         else:
+#             raise NotImplementedError('unknown story kind: "%s".' % kind)
+#
+#     def getGetIssueMessage(self, kind, name):
+#         #type: (Text, Optional[Text]) -> Text
+#         """
+#         Return an suitable issue message when using "getStory"
+#         and when there is no such story.
+#         To be used when getStory(kind, name) returned None.
+#         """
+#         if kind=='object model':
+#             return 'No object model provided. Include is invalid.'
+#         elif kind in ['context', 'story']:
+#             return ('Failed to include %s "%s".' % (kind, name))
+#         else:
+#             raise NotImplementedError('unknown story kind: "%s".' % kind)
+
+    # def getSetIssueMessage(self, kind, name):
+    #     #type: (Text, Optional[Text]) -> Text
+    #     """
+    #     Return an suitable issue message when using "setStory".
+    #     and when a story with that name is already defined.
+    #     To be used when getStory(kind, name) does not returned None.
+    #     """
+    #     if kind=='object model':
+    #         raise RuntimeError('Object model must not be set twice.')
+    #     elif kind in ['context', 'story']:
+    #         return ('%s %s is already defined".' % (kind, name))
+    #     else:
+    #         raise NotImplementedError('unknown story kind: "%s".' % kind)
+
 
 class StoryEvaluator(object):
     """
-    Engine performing the evaluation of a story. XXX ?
-    After creating the evaluator, use evaluateStory() to perform the
+    Engine performing the evaluation of a story.
+
+    After creating the evaluator with a given state,
+    "evaluateStory()" must be used to perform the
     evaluation itself.
+
+    The storyCollection given is used to interpret the
+    'include' statement.
+
+    The initialState parameter is used to initialize self.state.
+    This variable is then modified in place to the step executed
+    so that a typical usage is to give the state to be filled by
+    the story evaluator.
     """
 
-    def __init__(self, initialState, permissionSet=None):
-        #type: (ObjectModel) -> None
+    def __init__(self,
+                 initialState,
+                 storyCollection=None,
+                 permissionSet=None):
+        #type: (ObjectModel, AbstractStoryCollection, Optional[PermissionSet]) -> None
         """
         Create the evaluator object. Use evaluateStory() to launch
         the evaluation itself.
         :param initialState:
             The object model in which the story is evaluated.
             This object model will be updated during the evaluation.
+        :param storyCollection:
+            The environment used for include statements.
         :param permissionSet:
             A permissionSet to check the accesses against. If set
             some access Authorization/Denial can be created.
         """
+        self.storyCollection=storyCollection
+        #type: AbstractStoryCollection
+
         assert initialState is not None
         self.state=initialState
         #type: ObjectModel
@@ -119,9 +224,10 @@ class StoryEvaluator(object):
         #type: (Story) -> StepEvaluation
         """
         Evaluate a whole story and return a StoryEvaluation.
-        Create a story evaluation given an initial state (an object model).
+        Create a story evaluation given an initial state
+        (an object model).
         All steps in the story are executed one after the other.
-        The object model is modified in place, step after step.
+        The state self.state is modified in place, step after step.
         """
         self.storyEvaluation=(
             self._eval_step(story, parent=None))
@@ -130,7 +236,9 @@ class StoryEvaluator(object):
     def _eval_step(self, step, parent):
         #type: (Step, Optional[StepEvaluation]) -> StepEvaluation
         if isinstance(step, Story):
-            return self._eval_story(step)
+            return self._eval_story(step, parent)
+        elif isinstance(step, IncludeStep):
+            return self._eval_include(step, parent)
         elif isinstance(step, TextStep):
             return self._eval_text(step, parent)
         elif isinstance(step, VerbStep):
@@ -152,11 +260,55 @@ class StoryEvaluator(object):
             raise NotImplementedError(
                 'Unexpected step. type=:"%s"' % type(step))
 
-    def _eval_story(self, step):
-        seval=StoryEvaluation(step=step)
+    def _eval_story(self, step, parent):
+        #type: (Story, Optional[StoryIncludeEvaluation]) -> StoryEvaluation
+        seval=StoryEvaluation(step=step, parent=parent)
         for substep in step.steps:
             self._eval_step(substep, seval)
         return seval
+
+    def _eval_include(self, step, parent):
+        #---- (1) Create the (empty) Include Evaluation
+        include_evaluation=StoryIncludeEvaluation(
+            parent=parent,
+            step=step)
+
+        #---- (2) Get the story to be included
+        sc=self.storyCollection
+        story_to_included=\
+            sc.story(step.storyId)
+        if story_to_included is None:
+            ASTNodeSourceIssue(
+                code=icode('NO_STORY'),
+                astNode=step.astNode,
+                level=Levels.Fatal,
+                message='Include failed.')
+            # The message could be improved but only with
+            # various modifications. This would imply maintaining
+            # a map between StoryId (which is in a metamodel) and
+            # its external representation. The message above is
+            # good enough...
+            #
+            # This is a fatal issue. This means that
+            # the step commented below are not relevant:
+            #    step_eval.issues.append(i)
+            #    self.accesses=[]
+
+        #---- (3) Evaluate the selected story
+        # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxx
+        # TODO: sc-include12 must fail
+        #
+        # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxx
+
+        print('OO'*10, type(story_to_included))
+        story_evaluation_included=\
+            self._eval_story(
+                step=story_to_included,
+                parent=include_evaluation)
+        include_evaluation \
+            .storyEvaluationIncluded=story_evaluation_included
+        return include_evaluation
+
 
     def _eval_text(self, step, parent):
         return self._eval_composite(step, parent)
