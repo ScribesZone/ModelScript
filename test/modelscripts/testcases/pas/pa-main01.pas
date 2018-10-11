4777b21d629af3c337ea5e71d3053cb13f338a4f
escribis <escribis@users.noreply.github.com>
2018-10-11 10:26:26 +0200

    update participant according to new definition

--- test/modelscripts/testcases/pas/pa-main01.pas
+++ test/modelscripts/testcases/pas/pa-main01.pas
@@ -1,3 +1,13 @@
+-- Participant
+--     ParticipantClass
+--         Actor
+--         Stakeholder
+--         TeamRole
+--     ParticipantInstance
+--         Person
+--         Personage
+--         Personae
+
 participant model Demo
 
 actor Cashier
@@ -8,32 +18,38 @@ actor Client
 
 actor SpecialClient < Client
 
-personage marco : Cashier
+stakeholder StoreManager
+    | StoreManagers want the cash machine to
+    | collect the exact amount of money and
+    | to store safely this money.
+
+
+team role Developer
+    | A developer is responsible to develop and maintain
+    | some pecises of Code
+
+team role QualityManager
+    | The QualityManager is responsible to ensure that the
+    | quality of the product stay in line with quality
+    | requirements.
+
+team role CodeQualityManager < QualityManager
+    | A `CodeQualityManager` do something with code.
+
+team role ScrumMaster
+    | A scrum master is a facilitator in the context of
+    | a `ScrumTeam`
+
+team role ProductOwner
+
+person mariaStafani : Developer, QualityManager
+    trigram: MSI
+    | blabla blabla
+
+personae marco : Cashier
     | Marco is 32 years old.
     | He is used to computers and phones software.
 
 personage jean : Cashier, Client
     | Jean is 50 years old.
     | He is used to computers and phones software.
-
-role Developer
-    | A developer is responsible to develop and maintain
-    | some pecises of Code
-
-role QualityManager
-    | The QualityManager is responsible to ensure that the
-    | quality of the product stay in line with quality
-    | requirements.
-
-role CodeQualityManager < QualityManager
-    | A `CodeQualityManager` do something with code.
-
-role ScrumMaster
-    | A scrum master is a facilitator in the context of
-    | a `ScrumTeam`
-
-role ProductOwner
-
-participant mariaStafani : Developer, QualityManager
-    trigram: MSI
-    |
