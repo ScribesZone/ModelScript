a8b1268748e5ff771c69bfdd481298e0d8abc345
escribis <escribis@users.noreply.github.com>
2018-12-15 12:49:56 +0100

    add participants.pas

---
+++ methods/classroom-root/XXX-CASESTUDY/participants/participants.pas
@@ -0,0 +1,9 @@
+//=========================================================================
+//                      Participant model
+//-------------------------------------------------------------------------
+// This glossary model is expressed in ParticipantScript
+// See https://modelscript.rtfd.io/en/latest/scripts/participants/index.html
+//=========================================================================
+
+participant model XXX-CASESTUDY
+import glossary model from "../glossaries/glossaries.gls"
