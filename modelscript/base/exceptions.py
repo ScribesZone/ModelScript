c1842b375ab6b5a4e11a77f4f712cedd436a8d13
escribis <escribis@users.noreply.github.com>
2020-03-31 17:18:26 +0200

    Reformat

--- modelscript/base/exceptions.py
+++ modelscript/base/exceptions.py
@@ -1,28 +1,37 @@
 # coding=utf-8
 
+
 class NotFound(Exception):
     pass
+
 
 class InternalError(Exception):
     pass
 
+
 class UnexpectedCase(InternalError):
     pass
+
 
 class UnexpectedState(InternalError):
     pass
 
+
 class UnexpectedValue(InternalError):
     pass
+
 
 class NoSuchFeature(InternalError):
     pass
 
+
 class MethodToBeDefined(InternalError):
     pass
 
+
 class FileSystemError(InternalError):
     pass
+
 
 class TODO(InternalError):
     pass
