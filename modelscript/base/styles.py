c96fd9d599eda0055d9656f32d0fea3798486633
escribis <escribis@users.noreply.github.com>
2020-04-02 15:22:50 +0200

    Document

--- modelscript/base/styles.py
+++ modelscript/base/styles.py
@@ -1,7 +1,17 @@
+"""Styles used for pretty printing
+"""
+
+__all__ = (
+    'Style',
+    'Styles'
+)
+
 from modelscript.libs.termcolor import colored
 
 
 class Style(object):
+    """Style made of a color, a background and some attributes."""
+
     def __init__(self, c=None, b=None, a=None):
         self.color = c
         self.background = b
@@ -18,19 +28,20 @@ class Style(object):
 
 
 class Styles(object):
-    bigIssue=Style('red', a=['bold'])
-    mediumIssue=Style('magenta', a=['bold'])
-    smallIssue=Style('green')
-    bigIssueSummary=Style('red', a=['reversed','bold'])
-    smallIssueSummary=Style('magenta', a=['reversed','bold'])
+    """Libraru of predefined styles."""
 
-    keyword=Style('blue')
-    comment=Style('white')
-    highlight=Style('red')
-    annotate=Style('yellow')
-    s2=Style('green')
-    bold=Style(a=['bold'])
-    no=Style()
-    ko=Style('red')
-    ok=Style('green')
-    # underline, bold, reverse
+    bigIssue = Style('red', a=['bold'])
+    mediumIssue = Style('magenta', a=['bold'])
+    smallIssue = Style('green')
+    bigIssueSummary = Style('red', a=['reversed','bold'])
+    smallIssueSummary = Style('magenta', a=['reversed','bold'])
+
+    keyword = Style('blue')
+    comment = Style('white')
+    highlight = Style('red')
+    annotate = Style('yellow')
+    s2 = Style('green')
+    bold = Style(a=['bold'])
+    no = Style()
+    ko = Style('red')
+    ok = Style('green')
